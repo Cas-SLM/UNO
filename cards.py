51370e08bdab8e26ef69eb2efc9d81e379fb320c
Cas-SLM <smagwentshu99@gmail.com>
2024-09-20 20:38:30 +0200

    Encapsulated class fields

--- cards.py
+++ cards.py
@@ -8,22 +8,16 @@ def get_deck():
         match card_type:
             case "NUMBER":
                 [card_list.append(card) for card in get_number_cards()]
-                # card_list.append(get_number_cards())
             case "DRAW":
                 [card_list.append(card) for card in get_draw2_cards()]
-                # card_list.append(get_draw2_cards())
             case "REVERSE":
                 [card_list.append(card) for card in get_reverse_cards()]
-                # card_list.append(get_reverse_cards())
             case "SKIP":
                 [card_list.append(card) for card in get_skip_cards()]
-                # card_list.append(get_skip_cards())
             case "WILD":
                 [card_list.append(card) for card in get_wild_cards()]
-                # card_list.append(get_wild_cards())
             case "WILD DRAW":
                 [card_list.append(card) for card in get_wild_draw_cards()]
-                # card_list.append(get_wild_draw_cards())
     return card_list
 
 
@@ -86,26 +80,39 @@ def get_number_cards():
 
 
 class Card:
-    color = str
-    number = int
-    card_type = str
+    _color = str
+    _number = int
+    _card_type = str
     def __init__(self, color : str, number : int, card_type : str):
-        self.color = color
-        self.number = number
-        self.card_type = card_type
+        self._color = color
+        self._number = number
+        self._card_type = card_type
 
     def __str__(self):
-        if self.card_type == "NUMBER":
-            return f"{self.color} {self.number}"
-        elif self.card_type == "DRAW":
-            return f"{self.color} {self.card_type} {self.number}"
-        elif self.card_type in ["REVERSE", "SKIP"]:
-            return f"{self.color} {self.card_type}"
-        elif self.card_type == "WILD DRAW":
-            return f"{self.card_type} {self.number}"
-        elif self.card_type == "WILD":
-            return f"{self.card_type}"
+        if self._card_type == "NUMBER":
+            return f"{self._color} {self._number}"
+        elif self._card_type == "DRAW":
+            return f"{self._color} {self._card_type} {self._number}"
+        elif self._card_type in ["REVERSE", "SKIP"]:
+            return f"{self._color} {self._card_type}"
+        elif self._card_type == "WILD DRAW":
+            return f"{self._card_type} {self._number}"
+        elif self._card_type == "WILD":
+            return f"{self._card_type}"
         else:
-            return f"{self.card_type} {self.color} {self.number}"
+            return f"{self._card_type} {self._color} {self._number}"
 
+    def __repr__(self):
+        if self._card_type == "NUMBER":
+            return f"Card: {self._color} {self._number}"
+        elif self._card_type == "DRAW":
+            return f"Card: {self._color} {self._card_type} {self._number}"
+        elif self._card_type in ["REVERSE", "SKIP"]:
+            return f"Card: {self._color} {self._card_type}"
+        elif self._card_type == "WILD DRAW":
+            return f"Card: {self._card_type} {self._number}"
+        elif self._card_type == "WILD":
+            return f"Card: {self._card_type}"
+        else:
+            return f"Card: {self._card_type} {self._color} {self._number}"
 
--- player.py
+++ player.py
@@ -1,29 +1,83 @@
+from __future__ import annotations
+
 from cards import Card
 
 class Player:
-    player_type = str
-    player_name = str
-    hand = []
+    _player_type = str
+    _player_name = str
+    _hand = [Card]
+    _status = str
+    _next = None
+    _previous = None
 
-    def __init__(self, player_type : str, name : str):
-        self.player_type = player_type.upper()
-        self.player_name = name
-        self.hand = []
+    def __init__(self, player_type : str, name : str, hand : list[Card] = None):
+        self._player_type = player_type
+        self._player_name = name
+        self._hand = hand if hand else []
+        self._status = "ENGAGED"
+        self._next = None
+        self._previous = None
 
-    def __init__(self, player_type, name, hand):
-        self.player_type = player_type
-        self.player_name = name
-        self.hand = hand
+    def __str__(self):
+        if self._player_type == "COMPUTER":
+            return self._player_type
+        else:
+            return f"{self._player_name}"
 
-    def discard(self, card):
-        return self.hand.pop(self.hand.index(card))
+    def __repr__(self):
+        if self._player_type == "COMPUTER":
+            return f"Player: {self._player_type} - {self._player_name}"
+        else:
+            return f"Player: {self._player_name}"
+
+    @property
+    def __class__(self):
+        return self.__class__
+
+    def discard(self):
+        print(f"{self}'s cards:")
+        self.show_hand()
+        while True:
+            try:
+                index = int(input("Which card do you want to place?"))
+                if 0 < index <= len(self._hand):
+                    return self._hand.pop(index - 1)
+                else:
+                    raise AssertionError("Not enough cards for that number!")
+            except ValueError:
+                print("That's not a number!")
+            except AssertionError or IndexError:
+                print("That's not a in the list of cards")
+                self.show_hand()
+
+    def show_hand(self):
+        print(f"{self}'s cards:")
+        index = 1
+        for card in self._hand:
+            print(f"    {index}. {card}")
+            index += 1
 
     def add(self, *cards : Card):
         for card in cards:
-            self.hand.append(card)
+            self._hand.append(card)
 
-    def __str__(self):
-        if self.player_type == "COMPUTER":
-            return self.player_type
-        else:
-            return f"{self.player_name}"
+    def won(self):
+        return self._status == "WON"
+
+    def won_game(self):
+        self._status = "WON"
+
+    def set_next_player(self, player : Player):
+        self._next = player
+
+    def get_next_player(self):
+        return self._next
+
+    def set_previous_player(self, player : Player):
+        self._previous = player
+
+    def get_previous_player(self):
+        return self._previous
+
+    def lost_game(self):
+        self._status = "LOST"
